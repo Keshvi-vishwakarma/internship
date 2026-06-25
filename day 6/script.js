7a0ecadcdfb724c2d6df1538a42d06b276f43ab3
Keshvi-vishwakarma <keshvishwakarma2005@gmail.com>
2026-06-26 00:43:22 +0530

    initial6

--- day 6/script.js
+++ day 6/script.js
@@ -1,6 +1,42 @@
 const product=[
     {
-        name:"Americano",
-        decription:""
+        name:"Coffee",
+        decription:"Combines sensory tasting notes, the origin of the beans, and the perfect pour.",
+        price:"₹110",
+        image:"coffee.jpg",
+    },
+    {
+        name:"Pastas",
+        decription:"A comforting bowl of perfectly cooked pasta tossed in a rich, velvety sauce and infused with aromatic herbs, fresh garlic, and premium ingredients.",
+        price:"₹335",
+        image:"pasta.jpg",
+    },
+    {
+        name:"Sandwiches",
+        decription:"Freshly toasted bread filled with premium ingredients, crisp vegetables, and melted cheese for a deliciously satisfying bite.",
+        price:"₹259",
+        image:"sandwich.jpg",
+    },
+    {
+        name:"Desserts",
+        decription:"Handcrafted sweet treats made with premium ingredients, delivering the perfect balance of richness, flavor, and indulgence.",
+        price:"₹329",
+        image:"desserts.webp",
     }
-]
+];
+
+const cardContainer = document.getElementById("cardcontainer");
+
+const name='keshvi'
+cardcontainer.innerHTML =  
+product.map(
+    (product) => 
+      `
+        <div class="card">
+            <img src="${product.image}" alt="${product.name}" class="card-img">
+            <h2>${product.name}</h2>
+            <p>${product.decription}</p>
+            <p class="price">${product.price}</p>
+        </div>
+    `
+  );
